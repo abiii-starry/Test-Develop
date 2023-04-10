700efcd2fff669a5c6c77ac165cc414106637ead
lorraine <l96309354@gmail.com>
2023-04-10 11:44:29 +0800

    adjust directory structure

--- demo/utils.py
+++
@@ -1,5 +0,0 @@
-# 公共断言方法
-def common_assert(case, response, status_code=200, code=20000, status="success"):
-    case.assertEqual(status_code, response.status_code)
-    case.assertEqual(code, response.json().get("code"))
-    case.assertIn(status, response.json().get("status"))
